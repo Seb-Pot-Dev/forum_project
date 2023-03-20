2bf134a0a40434bbe2b27e2f27b05452ff47cd25
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-03-20 18:53:35 +0100

    added style to nav bar

--- view/layout.php
+++ view/layout.php
@@ -7,6 +7,9 @@
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rakkas&display=swap" rel="stylesheet">
     <title>FORUM</title>
 </head>
 <body>
@@ -19,7 +22,8 @@
             <header>
                 <nav>
                     <div id="nav-left">
-                        <a href="/">Accueil</a>
+                        <a href="index.php?ctrl=category&action=listCategories"><img src="https://cdn-icons-png.flaticon.com/512/1597/1597852.png" alt="logo de LA PLACE"></a>
+                        <a class="button-light" href="index.php?ctrl=category&action=listCategories">Toutes les categories</a>
                         <?php
                         // if(App\Session::isAdmin()){
                             ?>
@@ -29,6 +33,7 @@
                         // }
                         ?>
                     </div>
+                    <h1>La Place</h1>
                     <div id="nav-right">
                         <?php
                         
@@ -40,9 +45,8 @@
                         }
                         else{
                             ?>
-                            <a href="index.php?ctrl=category&action=listCategories">Liste des categories</a>
-                            <a href="./view/security/login.php">Connexion</a>
-                            <a href="/security/register.html">Inscription</a>
+                            <a class="button-dark" href="./view/security/login.php">Connexion</a>
+                            <a class="button-light" href="/security/register.html">Inscription</a>
                         <?php
                         }
                    
